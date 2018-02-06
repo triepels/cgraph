4a22d1531d8e8f299505ed9f0dc2f020bf9f67b4
rtriepels <rontriepels@hotmail.com>
2018-02-06 16:47:55 +0100

    Add wrapper function 'perm'.

---
+++ R/wrapper.R
@@ -0,0 +1,22 @@
+#' Array Transposition
+#'
+#' Transpose the row and columns of an array while keeping all dimensions the same.
+#'
+#' @section Usage:
+#' \preformatted{perm(a)}
+#'
+#' @section Agruments:
+#' \describe{
+#' \item{a}{array, the array to be transposed.}
+#' }
+#'
+#' @note This is a wrapper function for \code{aperm(a, replace(1:length(dim(a)), 1:2, 2:1), resize = F)}.
+#'
+#' @return array, transposed array.
+#'
+#' @name perm
+#' @author Ron Triepels
+perm <- function(a)
+{
+  return(aperm(a, replace(1:length(dim(a)), 1:2, 2:1), resize = F))
+}
