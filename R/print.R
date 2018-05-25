d78c90850e805454bc9caf71f8e506a3ac9e61e4
Ron Triepels <rontriepels@hotmail.com>
2018-05-25 07:59:24 +0200

    Function 'print.cg.node' now prints the value of a variable by running the computational graph.

--- R/print.R
+++ R/print.R
@@ -5,7 +5,25 @@ print.cgraph <- function(x, ...)
 
 print.cg.node = function(x, ...)
 {
-  cat(sprintf("<cg.node: %s> '%s'", .Call("cg_types")[attr(x, "type") + 1], x))
+  val <- NULL
+
+  if(!exists("graph", envir = .session))
+  {
+    stop("No current graph set")
+  }
+
+  tryCatch(
+  {
+    val <- get(x, envir = .session$graph$run(x))
+  },
+  error = function(e)
+  {
+    e$call <- NULL
+
+    warning(e)
+  })
+
+  print(val)
 }
 
 print.cg.environment <- function(x, ...)
