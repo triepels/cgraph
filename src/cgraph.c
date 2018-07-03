a66c0e3e8454e71d3cad9d7adf1e09ba06c04f7c
Ron Triepels <rontriepels@hotmail.com>
2018-07-03 18:17:13 +0200

    Fixed bugg in 'cg_add_expression'.

--- src/cgraph.c
+++ src/cgraph.c
@@ -174,11 +174,7 @@ SEXP cg_add_expression(SEXP call, SEXP grads, SEXP binding, SEXP name, SEXP grap
     setVar(install(CHAR(var)), coerceVector(value, SYMSXP), binding);
   }
 
-  SEXP parents = PROTECT(allocVector(INTSXP, m));
-
   SEXP names = getAttrib(grads, R_NamesSymbol);
-
-  n = LENGTH(nodes);
 
   m = LENGTH(grads);
 
@@ -190,6 +186,15 @@ SEXP cg_add_expression(SEXP call, SEXP grads, SEXP binding, SEXP name, SEXP grap
     {
       error("cannot find '%s' in binding", CHAR(STRING_ELT(names, i)));
     }
+  }
+
+  SEXP parents = PROTECT(allocVector(INTSXP, m));
+
+  n = LENGTH(nodes);
+
+  for(int i = 0; i < m; i++)
+  {
+    SEXP symbol = findVar(install(CHAR(STRING_ELT(names, i))), binding);
 
     int parent_id = cg_node_id(coerceVector(symbol, CHARSXP), graph);
 
