fb96f366f39cf4e81c6e31a697c7a5c912d43eb1
Ron Triepels <rontriepels@hotmail.com>
2018-08-10 14:52:05 +0200

    Reorganized some code.

--- src/stack.c
+++ src/stack.c
@@ -14,11 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <R.h>
 #include <Rinternals.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "stack.h"
 
