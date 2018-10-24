813f8ce5718644f4fa697c6858cbecb33570c6a9
Ron Triepels <rontriepels@hotmail.com>
2018-10-24 16:55:26 +0200

    Changed some code.

--- src/cgraph.c
+++ src/cgraph.c
@@ -949,9 +949,9 @@ SEXP cg_eval(SEXP node, SEXP values, SEXP graph)
     {
       SEXP parent = PROTECT(cg_get_node_id(node_parents[i], graph));
 
-      SEXP parent_value = PROTECT(Rf_eval(cg_get_symbol(parent), values));
+      //SEXP parent_value = PROTECT(Rf_eval(cg_get_symbol(parent), values));
 
-      SETCAR(arg, parent_value);
+      SETCAR(arg, cg_get_symbol(parent));
 
       UNPROTECT(2);
 
