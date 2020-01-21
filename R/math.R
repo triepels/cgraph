9fd1bee1ba8fa76aaa079eb97a8553cd829bcb51
triepels <rontriepels@hotmail.com>
2020-01-21 11:02:56 +0100

    Updated copyright note.

--- R/math.R
+++ R/math.R
@@ -1,4 +1,4 @@
-# Copyright 2019 Ron Triepels
+# Copyright 2020 Ron Triepels
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
