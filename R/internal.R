e5de64852e21aa04591869fa6cca08778ceb413b
triepels <rontriepels@hotmail.com>
2020-01-28 13:51:20 +0100

    Updated documentation of function dots.

--- R/internal.R
+++ R/internal.R
@@ -18,9 +18,9 @@
 #'
 #' @param env, environment, the environment in which to search for the ellipsis. Defaults to the parent frame.
 #'
-#' @note Any promise objects in the ellipsis object are evaluated in environment \code{env}.
+#' @note Any promise objects in the ellipsis object are evaluated in environment \code{env}. `NULL` is returned in case no ellipsis object is found in environment `env`.
 #'
-#' @return list containing all objects provided to the ellipsis object.
+#' @return list containing all objects provided to the ellipsis.
 #'
 #' @author Ron Triepels
 #' @keywords internal
