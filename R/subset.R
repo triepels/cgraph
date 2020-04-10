f2ea70307fa081cb23f4400c25de6b3886d49c68
triepels <rontriepels@hotmail.com>
2020-04-10 23:29:41 +0200

    Added operator cg_subassign1 and cg_subassign2.

--- R/subset.R
+++ R/subset.R
@@ -53,6 +53,51 @@ delayedAssign("subset1", cg_function(
   cg_subset1(x, ...)
 }
 
+#' Subassign
+#'
+#' Calculate \code{x[...] <- y}.
+#'
+#' @param x a cg_node object.
+#' @param ... either cg_node objects or numerical scalars that are passed on to the \code{`[<-`} function.
+#' @param y either a cg_node or an array of a similar class as \code{x}.
+#' @param name character scalar, name of the operation (optional).
+#'
+#' @return cg_operator object.
+#'
+#' @note This operator is not differentiable with respect to the arguments provided to \code{...}. Any attempt to differentiate this operator with respect to these arguments results in an error.
+#'
+#' @seealso \link[base:Extract]{subassign}
+#'
+#' @author Ron Triepels
+#' @export
+cg_subassign1 <- function(x, ..., y, name = NULL)
+{
+  cg_operator(subassign1, c(x = x, dots(), y = y))
+}
+
+# Function definition
+delayedAssign("subassign1", cg_function(
+  def = base::`[<-`,
+  grads = list(
+    x = function(x, ..., y, value, grad)
+    {
+      grad[...] <- 0
+      grad
+    },
+    y = function(x, ..., y, value, grad)
+    {
+      grad[...]
+    }
+  )
+))
+
+#' @export
+#' @author Ron Triepels
+`[<-.cg_node` <- function(x, ..., value)
+{
+  cg_subassign1(x, ..., y = value)
+}
+
 #' Subset
 #'
 #' Calculate \code{x[[...]]}.
@@ -92,4 +137,49 @@ delayedAssign("subset2", cg_function(
 `[[.cg_node` <- function(x, ...)
 {
   cg_subset2(x, ...)
+}
+
+#' Subassign
+#'
+#' Calculate \code{x[[i]] <- y}.
+#'
+#' @param x a cg_node object.
+#' @param i either a cg_node or a numerical scalar that is passed on to the \code{`[[<-`} function.
+#' @param y either a cg_node or an array of a similar class as \code{x}.
+#' @param name character scalar, name of the operation (optional).
+#'
+#' @return cg_operator object.
+#'
+#' @note This operator is not differentiable with respect to \code{i}. Any attempt to differentiate this operator with respect to this argument results in an error.
+#'
+#' @seealso \link[base:Extract]{subassign}
+#'
+#' @author Ron Triepels
+#' @export
+cg_subassign2 <- function(x, i, y, name = NULL)
+{
+  cg_operator(subassign1, list(x = x, i = i, y = y))
+}
+
+# Function definition
+delayedAssign("subassign2", cg_function(
+  def = base::`[[<-`,
+  grads = list(
+    x = function(x, i, y, value, grad)
+    {
+      grad[[...]] <- 0
+      grad
+    },
+    y = function(x, i, y, value, grad)
+    {
+      grad[[...]]
+    }
+  )
+))
+
+#' @export
+#' @author Ron Triepels
+`[[<-.cg_node` <- function(x, i, value)
+{
+  cg_subassign1(x, i, y = value)
 }
